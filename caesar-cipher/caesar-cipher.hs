5182c4672fbec4ef7adcb52a3894234e18367520
Lakshminarayanan Nandakumar <srinathln7@Lakshminarayanans-MacBook-Air.local>
2022-05-21 21:53:56 +0200

    modify caesar cipher algo to include uppercase letters

--- caesar-cipher/caesar-cipher.hs
+++ caesar-cipher/caesar-cipher.hs
@@ -10,29 +10,41 @@ import Data.List
 -- Map a given character to the index [0..25]
 -- For example, a -> index 0 ,   z -> index 25
 
-char2index :: Char -> Int
-char2index ch = ord ch - ord 'a'
+char2indexL :: Char -> Int
+char2indexL ch = ord ch - ord 'a'
 
 -- Convert a given index in the list [0..25] to the respective characters
 -- For example, index 7 -> 'h'
 
-index2char :: Int -> Char
-index2char n = chr (n + ord 'a')
+index2charL :: Int -> Char
+index2charL n = chr (n + ord 'a')
+
+-- Map a given character to the index [0..25]
+-- For example, A -> index 0 ,   Z -> index 25
+
+char2indexU :: Char -> Int
+char2indexU ch = ord ch - ord 'A' 
+
+
+-- Convert a given index in the list [25..51] to the respective characters
+-- For example, index 1 -> 'B'
+
+index2charU :: Int -> Char
+index2charU n = chr (n + ord 'A') 
 
 
 -- Shift a given character with the given shift factor
 -- +ve num means shifting right and -ve num means shifting left.
 
 shift :: Int -> Char -> Char
-shift n ch  | isLower ch =  index2char ((char2index ch + n) `mod` 26)
-            | otherwise = ch
+shift n ch  | isLower ch = index2charL ((char2indexL ch + n) `mod` 26)
+            | otherwise = index2charU((char2indexU ch + n ) `mod` 26) 
 
 
 -- Encoder function - given a shift factor and a string, encoder outputs the newly encoded string
 
 encode :: Int -> String -> String
 encode n xs = [ shift n x | x <- xs]
-
 
 
 -- Crack the caesar cipher
@@ -64,12 +76,14 @@ lowers xs = [ x | x <- xs, isAsciiLower x]
 positions :: Eq a => a -> [a] -> [Int]
 positions x xs = [i | (x', i)  <- zip xs [0 ..], x == x']
 
+
 -- Contruct a table analyzing the frequency of each letter from 'a' to 'z' in the given string
 freqs :: String -> [Float]
-freqs xs = [relativepercent (count x xs) n | x <- ['a'..'z']]
-            where n = length $lowers xs
-
-
+freqs xs = [relativepercent (count x xsl) nl | x <- ['a'..'z']]  
+            where 
+                xsl = lowers xs
+                nl = length xsl
+              
 -- Establish a method to choose the potenial shift factor with minimum error and maximum possibility
 -- chi-square statistic metod. simlilar to least-squares method
 
@@ -82,12 +96,26 @@ rotate :: Int -> [a] -> [a]
 rotate n xs = drop n xs ++ take n xs 
 
 
+crackL :: String -> String
+crackL xs = encode (-factor) xsl
+           where 
+               xsl = [toLower x | x <- xs]
+               factor =  head (positions (minimum chisqrDifftable) chisqrDifftable ) 
+               chisqrDifftable = [chisqr (rotate n table') table | n <- [0..25]] 
+               table' = freqs xs 
+
+
+-- make the characters in string uppercase at a given set of indices
+uppercaseAt :: [Int] -> String -> String
+uppercaseAt ns xs = [ if i `elem` ns then toUpper x else x | (x,i) <- zip xs [0..] ]
+
+-- Display the positions of the uppercase characters in the string
+positionsUp ::  String -> [Int]
+positionsUp  xs = [i | (x, i)  <- zip xs [0 ..], isAsciiUpper x]
+
+
 --  CRACK function
 -- decodes the potential original string with the given encoded string
 
 crack :: String -> String
-crack xs = encode (-factor) xs
-           where 
-               factor =  head (positions (minimum chisqrDifftable) chisqrDifftable ) 
-               chisqrDifftable = [chisqr (rotate n table') table | n <- [0..25]] 
-               table' = freqs xs    
+crack xs = uppercaseAt (positionsUp xs) $ crackL xs     
