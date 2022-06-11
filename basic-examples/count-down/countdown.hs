d63575c5fa84038b1703514ff48be08559caf317
srinathLN7 <lakshminarayanan.nandakumar@cgi.com>
2022-06-11 12:27:35 +0200

    implement complexity score for countdown problem and find nearest sol if no solution exists

--- basic-examples/count-down/countdown.hs
+++ basic-examples/count-down/countdown.hs
@@ -1,4 +1,6 @@
 module CountDown where 
+import Data.List (sortBy)
+import Data.Ord (comparing)
 
 --- Defining all possible operators
 data Op = Add | Sub | Mul | Div |Exp   
@@ -107,10 +109,66 @@ results list = [res |  (ls, rs) <- splitInToTwo list,
 solutions :: [Int] -> Int -> [Expr] 
 solutions list target = [ expr | ls <- choices list, (expr, value) <- results ls, value == target]
 
+-- IF SOLUTION TO A PROBLEM DOES NOT EXIST
 
-sol :: [Expr]
-sol = solutions [1,3,7,10,25,50] 765
+-- define a function that gets allpossible expressions and difference between the actual and target value
+allpossiblesoldiff :: [Int] -> Int -> [Result]
+allpossiblesoldiff list target = [(expr, abs (value - target))| ls <- choices list, (expr, value) <- results ls]
+
+-- sort all possible sol differences according to the absolute value of difference between the actual and target value
+sortallpossiblesol :: [Int] -> Int -> [Result]
+sortallpossiblesol list target =  sortBy (comparing snd) $ allpossiblesoldiff list target 
+
+-- Find the nearest solution by taking the head of the list
+nearestsolution :: [Int] -> Int -> Result
+nearestsolution list target = head (sortallpossiblesol list target)
+
+-- ORDERING SOLUTIONS USING COMPLEXITY
+
+type ResultsWithComplexity = [(Expr, Int)]
+type EfficientResult = (Expr, Int)
+
+
+getcomplexityscore :: Expr -> Int
+getcomplexityscore (Val n) = 0
+getcomplexityscore (App Add l r) = 1 + getcomplexityscore l + getcomplexityscore r
+getcomplexityscore (App Sub l r) = 1 + getcomplexityscore l + getcomplexityscore r
+getcomplexityscore (App Mul l r) = 10 + getcomplexityscore l + getcomplexityscore r
+getcomplexityscore (App Div l r) = 10 + getcomplexityscore l + getcomplexityscore r
+getcomplexityscore (App Exp l r) = 100 + getcomplexityscore l + getcomplexityscore r
+
+
+ordersolutionsbycomplexity :: [Int] -> Int -> ResultsWithComplexity
+ordersolutionsbycomplexity list target = sortBy (comparing snd) [(expr, getcomplexityscore expr) |  expr  <- solutions list target] 
+
+getsimplestsolution :: [Int] -> Int -> EfficientResult
+getsimplestsolution list target = head (ordersolutionsbycomplexity list target)  
+
+
+
+-- Examples for quick test
+
+sol1 :: [Expr]
+sol1 = solutions [1,3,7,10,25,50] 765
+
+sol2 :: EfficientResult
+sol2 = getsimplestsolution [1,3,7,10,25,50] 765
+
+-- no actual solution exists for this problem
+sol3 :: Result
+sol3 = nearestsolution [1,3,7,10,25,50] 831
+
 
 main :: IO ()
-main = do print sol 
-          print (length sol)   
+main = do putStr "Solutions for the problem:"
+          print sol1 
+          putStr "No of solutions that exist:"
+          print (length sol1)
+          putStr "Simplest solution:"
+          print (fst sol2)
+          putStr "with complexity score:" 
+          print (snd sol2) 
+          putStr "Nearest solution:"
+          print sol3
+          putStr "with absolute difference:" 
+          print (snd sol3)
