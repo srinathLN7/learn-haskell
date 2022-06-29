43a51e8d4a856ed1549a781d73b052be0d28cc74
srinathLN7 <lakshminarayanan.nandakumar@cgi.com>
2022-06-29 21:02:12 +0200

    add traversable examples

--- traversables/README.md
+++ traversables/README.md
@@ -2,4 +2,20 @@
 
 ### Output
 ```
+*Traversables> traverses dec [1,2,3]
+Just [0,1,2]
+*Traversables> traverses dec [0,1,2]
+Nothing
+*Traversables> Traversables.sequenceA [Just 1, Just 2, Just 3]
+Just [1,2,3]
+*Traversables> Traversables.sequenceA [Just 1, Just 2, Nothing]
+Nothing
+*Traversables> traverses dec (Node (Leaf 1) (Leaf 2))
+Just (Node (Leaf 0) (Leaf 1))
+*Traversables> traverses dec (Node (Leaf 0) (Leaf 1))
+Nothing
+*Traversables> Traversables.sequenceA (Node (Leaf (Just 1)) (Leaf (Just 2)))
+Just (Node (Leaf 1) (Leaf 2))
+*Traversables> Traversables.sequenceA (Node (Leaf (Just 0)) (Leaf (Just 1)))
+Just (Node (Leaf 0) (Leaf 1))
 ```
--- traversables/Traversables.hs
+++ traversables/Traversables.hs
@@ -1,7 +1,4 @@
 module Traversables where 
-
-
-
 class (Functor t , Foldable t ) => Traversables t where 
     traverses :: Applicative f => (a -> f b) -> t a -> f (t b)  
     
@@ -10,5 +7,41 @@ sequenceA ::(Traversables t,  Applicative f) => t (f a) -> f (t a)
 sequenceA = traverses  id 
 
 
--- traverses :: Applicative f => (a -> f b) -> t a -> f (t b) 
--- traverses = sequenceA . fmap g 
+instance Traversables [] where 
+    -- traverses :: Applicative f => (a -> f b) -> [a]-> f ([b]) 
+
+    traverses _ [] = pure []
+    traverses g (x:xs) = pure (:) <*> g x <*> traverses g xs  
+
+instance Traversables Maybe where
+    -- traverses :: Applicative f => (a -> f b) -> Maybe a -> f (Maybe b)
+    traverses _ Nothing = pure (Nothing)
+    traverses g (Just x) = pure (Just) <*> g x 
+
+
+data Tree a = Leaf a | Node (Tree a) (Tree a)  deriving Show 
+
+instance Functor Tree where
+    -- fmaps :: (a -> b) -> Tree a -> Tree b
+    fmap g (Leaf x) = Leaf (g x)
+    fmap g (Node l r) = Node (fmap g l) (fmap g r)
+instance Foldable Tree where 
+     -- foldMap :: Monoids b => (a -> b) -> Tree a -> b 
+    foldMap f (Leaf x) = f x 
+    foldMap f (Node l r) = foldMap f l `mappend` foldMap f r
+
+instance Traversables Tree   where
+    -- traverses :: Applicative f => (a -> f b) -> Tree a -> f (Tree b)
+    traverses g (Leaf x) = pure Leaf <*> g x
+    traverses g (Node l r) = pure Node <*> traverses g l <*> traverses g r  
+
+
+dec :: Int -> Maybe Int
+dec n = if n >0 then Just (n-1) else Nothing
+
+
+mapM :: (Traversables t ,Monad m) => (a -> m b) -> t a -> m (t b)
+mapM = traverses
+
+sequenceM :: (Traversables t ,Monad m) => t (m a) -> m (t a)
+sequenceM = Traversables.sequenceA 
